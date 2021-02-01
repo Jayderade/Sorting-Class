7a78cc790ae241fb0b427833eaa6b7c085d9a3eb
Jayderade <30771579+Jayderade@users.noreply.github.com>
2021-02-01 16:20:56 +1100

    Sorting

---
+++ Assets/Sorting/Scripts/RadixSorter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Sorting;
+using Array = System.Array;
+
+public class RadixSorter : BaseSorter
+{
+    protected override IEnumerator Sort()
+    {
+        int nodeCount = nodes.Length;
+        int i, j;
+        Node[] temp = new Node[nodes.Length];
+
+        for(int shift = 31; shift > -1; --shift)
+        {
+            // Reset j to zero
+            j = 0;
+
+            // Loop through the whole array
+            for (i = 0; i < nodeCount; ++i)
+            {
+                // Determine if the bit shifted variable is above 0
+                bool move = (nodes[i].Index << shift) >= 0;
+                if (shift == 0 ? !move : move)
+                    nodes[i - j] = nodes[i];
+                else
+                    temp[j++] = nodes[i];
+            }
+
+            // Copy the data to the temp array
+            Array.Copy(temp, 0, nodes, nodes.Length - j, j);
+
+            // Stupid visualisation
+            StartFrame(0, 1);
+            yield return null;
+            EndFrame(0, 1);
+
+        }
+    }
+}
--- Assets/Sorting/Scripts/SelectionSorter.cs
+++ Assets/Sorting/Scripts/SelectionSorter.cs
@@ -8,6 +8,39 @@ public class SelectionSorter : BaseSorter
 {
     protected override IEnumerator Sort()
     {
-        
+        int nodeCount = nodes.Length;
+        int smallest; // This is actually the item index... NOT THE ACTUAL VALUE
+        int old; // This is not part of the algorithm, just visaulisation
+        Node tempNode;
+
+        // Looping through all elements
+        for(int i = 0; i < nodeCount - 1; i++)
+        {
+            // Assign the smallest number to this loops iterater value
+            smallest = i;
+            old = i;
+
+            // Loop from this iteration to the end
+            for(int j = i + 1; j < nodeCount; j++)
+            {
+                // Compare this index to the current smallest index item.
+                if(nodes[j].Index < nodes[smallest].Index)
+                {
+                    smallest = j;
+                }
+            }
+
+            // Swap the elements
+            tempNode = nodes[smallest];
+            nodes[smallest] = nodes[old];
+            nodes[old] = tempNode;
+
+
+            // -- SIMPLY VISUALISATION NOT PART OF ALGORITHM -- ///
+            StartFrame(old, smallest);
+            yield return null;
+            EndFrame(old, smallest);
+
+        }
     }
 }
